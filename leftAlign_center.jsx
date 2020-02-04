56ebb3d8eb467a1a121d3b1c41fd3615bcef58c8
Neko6 <48623468+darlingcats@users.noreply.github.com>
2020-02-04 17:28:18 +0900

    Update leftAlign_center.jsx

--- leftAlign_center.jsx
+++ leftAlign_center.jsx
@@ -1,16 +1,26 @@
-var sel = app.selection[0];
-var col = sel.columns;
-sel.leftInset = sel.rightInset = 0;
-for (var i = 0, iL = col.length; i < iL; i++) {
-  var cel = col[i].cells;
-  var colW = col[i].width;
-  var cr = [];
-  for (var j = 1, jL = cel.length; j < jL; j++) {
-    var os_start = cel[j].lines[0].insertionPoints[0].horizontalOffset;
-    var os_end = cel[j].lines[0].insertionPoints[-1].horizontalOffset;
-    cr.push((colW - (os_end - os_start)) / 2);
-    cel[j].paragraphs[0].justification = Justification.leftAlign;
+Main();
+function Main() {
+  app.doScript("doMain()", ScriptLanguage.JAVASCRIPT, [], UndoModes.fastEntireScript);
+}
+function doMain() {
+  function cellWidth(cell) {
+    var points = cels[j].lines[0].insertionPoints;
+    return points[-1].horizontalOffset - points[0].horizontalOffset;
   }
-  col[i].textLeftInset = Math.round(cr.sort(function(a, b) {return a - b;})[0] * 100) / 100;
-  cel[0].textLeftInset = cel[0].textRightInset = 1;
+  var sel = app.selection[0];
+  var cols = sel.columns; // ★colsが複数形って名前でわかるようにしておきたい
+  sel.leftInset = sel.rightInset = 0;
+
+  var i, iL, j, jL, cels, colWidth, minCellInset;
+  for (i = 0, iL = cols.length; i < iL; i++) {
+    cels = cols[i].cells;
+    colWidth = cols[i].width;
+    minCellInset = Infinity; // ★Infinityは大きな値を示す組込の変数
+    for (j = 1, jL = cels.length; j < jL; j++) {
+      minCellInset = Math.min(minCellInset, colWidth - cellWidth(cels[j]));
+      cels[j].paragraphs[0].justification = Justification.leftAlign;
+    }
+    cols[i].textLeftInset = Math.round((minCellInset / 2) * 100) / 100;
+    cels[0].textLeftInset = cels[0].textRightInset = 1; //ヘッダー
+  }
 }
