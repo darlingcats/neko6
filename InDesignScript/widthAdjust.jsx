374837092266cc66f9757d639c526d588b94e861
Neko6 <48623468+darlingcats@users.noreply.github.com>
2020-04-06 17:56:56 +0900

    Update widthAdjust.jsx

--- InDesignScript/widthAdjust.jsx
+++ InDesignScript/widthAdjust.jsx
@@ -1,14 +1,23 @@
+//v1_7 20406 改行に対応・「入数」、「あたりの価格」改良
+
 Main();
 function Main() {
   app.doScript("doMain()", ScriptLanguage.JAVASCRIPT, [], UndoModes.fastEntireScript);
 }
 function doMain() {
   var doc = app.activeDocument;
-  var lab = "selec"; //商品詳細表スクリプトラベル
+  var lab = "productTable"; //商品詳細表スクリプトラベル
   var lab2 = "makerName"; //メーカー名、商品名スクリプトラベル
 
+  var x1, y1, x2, y2;
+  x1 = 0;
+  y1 = 0;
+  x2 = 350;
+  y2 = 220;
+
   //　ダイアログ作成
-  var objDlg = new Window("dialog", "検索文字列で列幅変更", [0, 0, 350, 220]);
+  var objDlg = new Window("dialog", "同じ内容のセルを連結", [x1, y1, x2, y2]);
+
   //　テキストを追加
   var objStText01 = objDlg.add("statictext", [20, 25, 350, 45], "検索する文字列");
   var objStText02 = objDlg.add("statictext", [20, 95, 350, 115], "列幅の数値");
@@ -16,26 +25,23 @@ function doMain() {
   var objTxtbox01 = objDlg.add("edittext", [20, 50, 315, 75], "");
   var objTxtbox02 = objDlg.add("edittext", [20, 120, 315, 145], "");
   //ボタン
-  objDlg.add("button", [340 - 145, 220 - 40, 340 - 95, 220 - 15], "実行", { name: "ok" });
-  objDlg.add("button", [340 - 90, 220 - 40, 340 - 10, 220 - 15], "キャンセル", { name: "cancel" });
+  objDlg.add("button", [x2 - 145, y2 - 40, x2 - 95, y2 - 15], "実行", { name: "ok" });
+  objDlg.add("button", [x2 - 90, y2 - 40, x2 - 10, y2 - 15], "キャンセル", { name: "cancel" });
   //　ダイアログ表示
   objDlg.center();
   var rtType = objDlg.show();
   //　ダイアログボックスの戻り値から条件分岐して、値を表示
 
-  var searchLabel01 = "あたりの価格"; //固定列幅用キーワード01
-  var searchLabel01val = "12mm"; //固定列用列幅01
-  var searchLabel02 = /入数\/\r/; //固定列幅用キーワード02
-  var searchLabel02val = "8mm"; //固定列用列幅02
+  var searchLabel01 = "あたりの価格"; //「あたりの価格」は無視
 
   if (rtType == 1) {
     alert("検索した文字列の列幅を変更します" + "\r\n\r\n" + "検索する文字列 ： " + objTxtbox01.text + "\r\n" + "列幅 ： " + objTxtbox02.text, "処理終了");
 
-    const startTime = Date.now(); // 開始時間
+    //const startTime = Date.now(); // 開始時間
 
     var widths, x, xL, tabs, y, yL, myTable, cols, w, v, myCellName, myCellNamePre, myCell, myCellPre, resul, resulPre;
     for (x = 0, xL = doc.textFrames.length; x < xL; x++) {
-      widths = [];
+      widths = {};
       tabs = doc.textFrames[x].tables;
       for (y = 0, yL = tabs.length; y < yL; y++) {
         if (tabs[y].columns.length > 0) {
@@ -51,18 +57,6 @@ function doMain() {
                   myCellPre = myTable.cells.itemByName(myCellNamePre); //対象セル - 1列名
                   resul = myCell.contents;
                   resulPre = myCellPre.contents;
-                  if (resul.search(searchLabel01) != -1) {
-                    if (resul != resulPre) {
-                      //横結合セルだった場合（左隣の内容と同じだったら無視）
-                      widths[w] = searchLabel01val;
-                    }
-                  }
-                  if (resul.search(searchLabel02) != -1) {
-                    if (resul != resulPre) {
-                      //横結合セルだった場合（左隣の内容と同じだったら無視）
-                      widths[w] = searchLabel02val;
-                    }
-                  }
 
                   if (objTxtbox01.text == "") {
                     //空欄だった場合
@@ -140,22 +134,31 @@ function doMain() {
           if (cels[j].texts[0].contents === "") {
             continue;
           }
+          if (cels[j].texts[0].contents.search(searchLabel01) != -1) {
+            continue;
+          }
           // if cell overflows
           if (cels[j].overflows) {
             while (cels[j].overflows) {
               cels[j].width += 1;
-              if (cels[j].width > 100) {
-                alert("内容に改行が入っている可能性があります");
-                return;
+              if (cels[j].lines.length > 1) {
+                while (cels[j].overflows) {
+                  cels[j].height += 2.5;
+                  if (cels[j].height > 8) {
+                    break;
+                  }
+                }
               }
-              if (cel[j].properties["lines"] !== undefined) {
+              if (cels[j].properties["lines"] !== undefined) {
                 break;
               }
             }
           }
-          var os_start = cels[j].lines[0].insertionPoints[0].horizontalOffset;
-          var os_end = cels[j].lines[0].insertionPoints[-1].horizontalOffset;
-          ar.push(os_end - os_start);
+          for (cL = 0, cLL = cels[j].lines.length; cL < cLL; cL++) {
+            var os_start = cels[j].lines[cL].insertionPoints[0].horizontalOffset;
+            var os_end = cels[j].lines[cL].insertionPoints[-1].horizontalOffset;
+            ar.push(os_end - os_start);
+          }
         }
         if (insertRIghtInset) {
           cols[i].rightInset = 2 * inset;
@@ -179,9 +182,9 @@ function doMain() {
       }
     }
 
-    const endTime = Date.now(); // 終了時間
+    //const endTime = Date.now(); // 終了時間
 
-    alert(endTime - startTime); // 何ミリ秒かかったかを表示する
+    //alert(endTime - startTime); // 何ミリ秒かかったかを表示する
 
     alert("処理が完了しました", "処理終了", true);
   } else if (rtType == 2) {
