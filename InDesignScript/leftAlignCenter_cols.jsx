ccb5f22cdcb85c6a401e1121bfc61e51c1f2e9f6
Neko6 <darling_cats@yahoo.co.jp>
2020-10-13 17:45:54 +0900

    Update leftAlignCenter_cols.jsx

--- InDesignScript/leftAlignCenter_cols.jsx
+++ InDesignScript/leftAlignCenter_cols.jsx
@@ -53,7 +53,7 @@ function doMain() {
                 }
             }
             cols[i].textLeftInset = Math.round((minCellInset) * 100) / 100;
-            cels[0].textLeftInset = cels[0].textRightInset = 1; //ヘッダー
+            cels[0].textLeftInset = cels[0].textRightInset = 0.5; //ヘッダー
         }
         if (overTxt > 0) {
             alert("オーバーテキストが" + overTxt + "箇所あります", "処理終了", true);
@@ -78,7 +78,7 @@ function doMain() {
                 }
             }
             cols[i].textLeftInset = Math.round((minCellInset / 2) * 100) / 100;
-            cels[0].textLeftInset = cels[0].textRightInset = 1; //ヘッダー
+            cels[0].textLeftInset = cels[0].textRightInset = 0.5; //ヘッダー
         }
         if (overTxt > 0) {
             alert("オーバーテキストが" + overTxt + "箇所あります", "処理終了", true);
